1a26ce8822194f5fbd0a97eff7dcd47693f9e863
deadpool395 <danieldas395@gmail.com>
2024-08-06 08:44:05 +0530

    Update index.js

--- projects/Drum-kit/index.js
+++ projects/Drum-kit/index.js
@@ -1,28 +1,28 @@
 function check(buttonclicked){
     buttonAnimation(buttonclicked);
     switch  (buttonclicked) {
-        case "w":  var tom1 = new Audio("./Sounds/tom-1.mp3"); 
+        case "w":  var tom1 = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/tom-1.mp3"); 
                     tom1.play();
                     
                     break;
-        case "a": var tom2 = new Audio("./Sounds/tom-2.mp3"); 
+        case "a": var tom2 = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/tom-2.mp3"); 
                     tom2.play();
                     break;
-        case "s": var tom3 = new Audio("./Sounds/tom-3.mp3"); 
+        case "s": var tom3 = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/tom-3.mp3"); 
                     tom3.play();
                      break;
-        case "d":  var tom4 = new Audio("./Sounds/tom-4.mp3"); 
+        case "d":  var tom4 = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/tom-4.mp3"); 
                     tom4.play();
                      break;
 
-        case "j": var snare = new Audio("./Sounds/snare.mp3"); 
+        case "j": var snare = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/snare.mp3"); 
                         snare.play();
                     break;
-        case "k": var crash = new Audio("./sounds/crash.mp3"); 
+        case "k": var crash = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/crash.mp3"); 
                     crash.play();
                     break;
 
-        case "l": var kick = new Audio("./Sounds/kick-bass.mp3"); 
+        case "l": var kick = new Audio("https://github.com/deadpool395/html-portfolio/raw/main/projects/Drum-kit/sounds/kick-bass.mp3"); 
                         kick.play();
                      break;
 
